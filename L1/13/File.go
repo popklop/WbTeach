305b1475123216b9d4e02ef792801b3b0c62476a
Abdul_Kamaz <popklop2280@gmail.com>
2025-08-22 17:34:59 +0300

    Init

---
+++ L1/13/File.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 44, 23
+	//Операциями
+	//a = a + b
+	//b = a - b
+	//a = a - b
+	//Множ. присваивание
+	//a, b = b, a
+	fmt.Println(a, b)
+
+}
