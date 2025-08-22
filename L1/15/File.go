b8d60bb0c61387fd559c05f31d7cbe25d0de6482
Abdul_Kamaz <popklop2280@gmail.com>
2025-08-22 23:26:36 +0300

    Init

---
+++ L1/15/File.go
@@ -0,0 +1,17 @@
+package main
+
+var justString string
+
+func someFunc() {
+	v := createHugeString(1000)
+	newString := make([]byte, 100)
+	copy(newString, v[:100])
+	justString = string(newString)
+	//Также можно вручную освободить память занимаемое этими переменными
+	newString = nil
+	v = ""
+}
+
+func main() {
+	someFunc()
+}
